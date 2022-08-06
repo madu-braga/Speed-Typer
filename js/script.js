7f4b9942b37e1487b83535f58688d97cefc10af0
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-08-05 21:35:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,28 @@ const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
+
+// words
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+  ];
+  
